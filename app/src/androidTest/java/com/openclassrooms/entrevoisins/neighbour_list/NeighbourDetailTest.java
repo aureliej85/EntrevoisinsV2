0680de9718a2c293d30f8309c2e648b65330dc02
aureliej85 <aureliejosephine@hotmail.com>
2020-01-26 21:46:30 +0100

    add comments

--- app/src/androidTest/java/com/openclassrooms/entrevoisins/neighbour_list/NeighbourDetailTest.java
+++ app/src/androidTest/java/com/openclassrooms/entrevoisins/neighbour_list/NeighbourDetailTest.java
@@ -16,16 +16,13 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
-import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.core.IsNull.notNullValue;
+
 
 public class NeighbourDetailTest {
 
-    private static int idNeighbourToTest = 6;
 
     private NeighbourApiService mApiService;
     private int mPosition = 2;
